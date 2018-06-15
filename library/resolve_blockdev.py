5d14a9f71731d5b41362ef86f6bc4f29c16bc28c
David Lehman <dlehman@redhat.com>
2018-06-15 12:20:25 -0400

    Refactor resolve_blockdev to enable unit testing.
    Also add support for resolving by basenames for device-mapper, md,
    and /dev/disk/ symlink.

--- library/resolve_blockdev.py
+++ library/resolve_blockdev.py
@@ -1,8 +1,97 @@
 #!/usr/bin/python
 
+ANSIBLE_METADATA = {
+    'metadata_version': '1.1',
+    'status': ['preview'],
+    'supported_by': 'community'
+}
+
+DOCUMENTATION = '''
+---
+module: resolve_blockdev
+short_description: Resolve block device specification to device node path.
+version_added: "2.5"
+description:
+    - "This module accepts various forms of block device identifiers and
+       resolves them to the correct block device node path."
+options:
+    spec:
+        description:
+            - String describing a block device
+        required: true
+author:
+    - David Lehman (dlehman@redhat.com)
+'''
+
+EXAMPLES = '''
+- name: Resolve device by label
+  resolve_blockdev:
+    spec: LABEL=MyData
+
+- name: Resolve device by name
+  resolve_blockdev:
+    spec: mpathb
+
+- name: Resolve device by /dev/disk/by-id symlink name
+  resolve_blockdev:
+    spec: wwn-0x5000c5005bc37f3f
+'''
+
+RETURN = '''
+device:
+    description: Path to block device node
+    type: str
+'''
+
+import glob
+import os
+import re
+
 from ansible.module_utils.basic import AnsibleModule
 
-import os
+DEV_MD = "/dev/md"
+DEV_MAPPER = "/dev/mapper"
+SYS_CLASS_BLOCK = "/sys/class/block"
+SEARCH_DIRS = ['/dev', DEV_MAPPER, DEV_MD] + glob.glob("/dev/disk/by-*")
+MD_KERNEL_DEV = re.compile(r'/dev/md\d+(p\d+)?$')
+
+
+def resolve_blockdev(spec, run_cmd):
+    if "=" in spec:
+        device = run_cmd("blkid -t %s -o device" % spec)[1].strip()
+    elif not spec.startswith('/'):
+        for devdir in SEARCH_DIRS:
+            device = "%s/%s" % (devdir, spec)
+            if os.path.exists(device):
+                break
+            else:
+                device = ''
+    else:
+        device = spec
+
+    if not device or not os.path.exists(device):
+        return ''
+
+    return canonical_device(os.path.realpath(device))
+
+
+def _get_dm_name_from_kernel_dev(kdev):
+    return open("%s/%s/dm/name" % (SYS_CLASS_BLOCK, os.path.basename(kdev))).read().strip()
+
+
+def _get_md_name_from_kernel_dev(kdev):
+    minor = os.minor(os.stat(kdev).st_rdev)
+    return next(name for name in os.listdir(DEV_MD)
+                    if os.minor(os.stat("%/%s" % (DEV_MD, name).st_rdev) == minor))
+
+
+def canonical_device(device):
+    if device.startswith("/dev/dm-"):
+        device = "%s/%s" % (DEV_MAPPER, _get_dm_name_from_kernel_dev(device))
+    elif MD_KERNEL_DEV.match(device):
+        device = "%s/%s" % (DEV_MD, _get_md_name_from_kernel_dev(device))
+    return device
+
 
 def run_module():
     module_args = dict(
@@ -18,16 +107,12 @@ def run_module():
         supports_check_mode=True
     )
 
-    if "=" in module.params['spec']:
-        result['device'] = os.popen("blkid -t %s -o device" % module.params['spec']).read().strip()
-    elif not module.params['spec'].startswith('/'):
-        result['device'] = "/dev/%s" % module.params['spec']
-        if not os.path.exists(result['device']):
-            result['device'] = None
-    else:
-        result['device'] = module.params['spec']
+    try:
+        result['device'] = resolve_blockdev(module.params['spec'], run_cmd=module.run_command)
+    except Exception:
+        pass
 
-    if not os.path.exists(result['device']):
+    if not result['device'] or not os.path.exists(result['device']):
         module.fail_json(msg="The {} device spec could not be resolved".format(module.params['spec']))
 
     module.exit_json(**result)
