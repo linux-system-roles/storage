4333cddd09386165effb41c53e51588d90cb1d42
Jan Pokorny <japokorn@redhat.com>
2020-08-07 15:23:46 +0200

    Changed size.py path
    - moved size.py to module_utils/storage
    - modified affected imports

--- library/bsize.py
+++ library/bsize.py
@@ -52,7 +52,7 @@ parted:
 '''
 
 from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.size import Size
+from ansible.module_utils.storage.size import Size
 
 def run_module():
     # available arguments/parameters that a user can pass
